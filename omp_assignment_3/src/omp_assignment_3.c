220536dff25c3ba2d026309193d4254b750fbba0
Tal Levin <talevin2000@gmail.com>
2022-05-03 13:41:57 +0300

    First commit

---
+++ omp_assignment_3/src/omp_assignment_3.c
@@ -0,0 +1,23 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[]) {
+
+  int numThreads, tid;
+
+#pragma omp parallel private(numThreads, tid)
+ {
+   tid = omp_get_thread_num();
+   printf("Hello World from thread number %d\n", tid);
+
+   if (tid == 0)
+     {
+       numThreads = omp_get_num_threads();
+       printf("Number of threads is %d\n", numThreads);
+     }
+ }
+ return 0;
+}
+
+
